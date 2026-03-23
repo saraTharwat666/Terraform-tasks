160e248198bd34df62af9b8c06c4826976990a72
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2026-03-23 14:11:22 +0200

    Add variables for S3 bucket and Firehose stream

---
+++ Building a Real-Time Data Ingestion Pipeline with Kinesis Firehose Using Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "KKE_S3_BUCKET_NAME" {
+  default = "nautilus-stream-bucket-15175"
+}
+
+variable "KKE_FIREHOSE_STREAM_NAME" {
+  default = "nautilus-firehose-stream"
+}
+
+variable "KKE_FIREHOSE_ROLE_NAME" {
+  default = "firehose-sts-role"
+}
