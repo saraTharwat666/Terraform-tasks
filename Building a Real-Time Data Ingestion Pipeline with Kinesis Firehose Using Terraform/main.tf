11aa2909b39e3e9e9550e0a603b5ced82d87d266
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2026-03-23 14:10:27 +0200

    Create main.tf

---
+++ Building a Real-Time Data Ingestion Pipeline with Kinesis Firehose Using Terraform/main.tf
@@ -0,0 +1,64 @@
+
+resource "aws_s3_bucket" "stream_bucket" {
+  bucket = var.KKE_S3_BUCKET_NAME
+}
+
+
+resource "aws_iam_role" "firehose_role" {
+  name = var.KKE_FIREHOSE_ROLE_NAME
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "firehose.amazonaws.com" }
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy" "firehose_s3_policy" {
+  name = "firehose-s3-access"
+  role = aws_iam_role.firehose_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action   = ["s3:PutObject", "s3:ListBucket", "s3:GetBucketLocation"]
+      Effect   = "Allow"
+      Resource = [
+        aws_s3_bucket.stream_bucket.arn,
+        "${aws_s3_bucket.stream_bucket.arn}/*"
+      ]
+    }]
+  })
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "nautilus_stream" {
+  name        = var.KKE_FIREHOSE_STREAM_NAME
+  destination = "extended_s3"
+
+  extended_s3_configuration {
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = aws_s3_bucket.stream_bucket.arn
+    
+
+    buffer_size     = 5
+    buffer_interval = 300
+
+
+    processing_configuration {
+      enabled = true
+      processors {
+        type = "AppendDelimiterToRecord"
+        parameters {
+          parameter_name  = "Delimiter"
+          parameter_value = "\\n"
+        }
+      }
+    }
+  }
+
+  depends_on = [aws_iam_role_policy.firehose_s3_policy]
+}
