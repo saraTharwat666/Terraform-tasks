8696bc02f7e312a4217762bdb14c5050e3b8ce4f
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2026-03-27 18:02:47 +0200

    Create variables.tf

---
+++ Deploying a Multi-Tier Architecture on AWS Using Terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "KKE_ENVIRONMENT" {
+  type = string
+}
+
+variable "KKE_DYNAMODB_TABLE_NAME" {
+  type = string
+}
+
+variable "KKE_SNS_TOPIC_NAME" {
+  type = string
+}
+
+variable "KKE_SSM_PARAM_NAME" {
+  type = string
+}
