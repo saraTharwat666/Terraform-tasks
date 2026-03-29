147d05842237525aa263ff1b9a4cfe82277060ea
Sara Tharwat <208049124+saraTharwat666@users.noreply.github.com>
2026-03-29 11:17:11 +0200

    Create variables.tf

---
+++ Alerting in Pipelines Using Terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "KKE_STAGING_BUCKET_NAME" { type = string }
+variable "KKE_FIREHOSE_ROLE_NAME" { type = string }
+variable "KKE_FIREHOSE_POLICY_NAME" { type = string }
+variable "KKE_FIREHOSE_NAME" { type = string }
+variable "KKE_SNS_TOPIC_NAME" { type = string }
+variable "KKE_CLOUDWATCH_ALARM_NAME" { type = string }
+variable "KKE_ALERT_EMAIL" { type = string }
